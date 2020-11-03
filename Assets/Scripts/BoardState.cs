f29c69f83f4a7604960192a1e5c0f4a6320c874b
Missing-Tech <josephgrabski31@gmail.com>
2020-11-03 18:36:27 +0000

    Commented stuff
    BoardState.cs + MinimaxAI.cs

--- Assets/Scripts/BoardState.cs
+++ Assets/Scripts/BoardState.cs
@@ -8,18 +8,17 @@ using UnityEditorInternal.VersionControl;
 using UnityEngine;
 using static Colours.ColourNames;
 
+/// <summary>
+/// This class is used for future board states so that the AI can predict the best move for the future
+/// Creates children classes for itself
+/// </summary>
 public class BoardState
 {
     private Cell[,] _cellGrid = new Cell[8, 8];
-
     public Cell[,] CellGrid => _cellGrid;
-
     private BoardState _parentState;
-
     public BoardState ParentState => _parentState;
-
     public List<BoardState> childrenStates;
-
     public Piece pieceToMove;
     public Cell cellToMove;
 
@@ -29,6 +28,9 @@ public class BoardState
         {
             if (childrenStates == null || childrenStates.Count == 0)
             {
+                //If there isn't any children when it's referenced then create some
+                //More performant than creating every single necessary child at once
+                //Only called when necessary
                 return CreateChildrenStates();
             }
 
@@ -38,6 +40,15 @@ public class BoardState
 
     public int _depth, _maxDepth;
 
+    /// <summary>
+    /// Constructor for future board states
+    /// </summary>
+    /// <param name="maxDepth">Limiting depth to stop the search from going on forever</param>
+    /// <param name="depth">The depth of this board state (how many moves ahead)</param>
+    /// <param name="parentState">Stores the board state that this is a child of</param>
+    /// <param name="cellGrid">Stores a local representation of the board</param>
+    /// <param name="pieceToMove">Stores the piece object to move(local to this board state)</param>
+    /// <param name="cellToMove">Stores the cell to move to(local to this board state)</param>
     private BoardState(int maxDepth, int depth, BoardState parentState,
         Cell[,] cellGrid, Piece pieceToMove, Cell cellToMove)
     {
@@ -47,14 +58,14 @@ public class BoardState
         this.pieceToMove = pieceToMove;
         this.cellToMove = cellToMove;
         _parentState = parentState;
-        
-        /*if (depth == 1)
-        {
-            _parentState.pieceToMove = pieceToMove;
-            _parentState.cellToMove = cellToMove;
-        }*/
     }
 
+    /// <summary>
+    /// Constructor for the current board state 
+    /// </summary>
+    /// <param name="maxDepth">Limiting depth to stop the search from going on forever</param>
+    /// <param name="cellGrid">Stores a local representation of the board</param>
+    /// <param name="depth">The depth of this board state (how many moves ahead)</param>
     public BoardState(int maxDepth, Cell[,] cellGrid, int depth)
     {
         _maxDepth = maxDepth;
@@ -65,6 +76,10 @@ public class BoardState
         _cellGrid = cellGrid;
     }
 
+    /// <summary>
+    /// Loops through every piece and creates a board state for every possible move
+    /// </summary>
+    /// <returns>Returns a list of future board states from this board state</returns>
     public List<BoardState> CreateChildrenStates()
     {
         List<BoardState> localChildrenStates = new List<BoardState>();
@@ -86,12 +101,6 @@ public class BoardState
                     {
                         Cell[,] newCellGrid = new Cell[8,8];
                         newCellGrid = _cellGrid;
-                        /*Cell newCellPos = newCellGrid[availableCell.cellPos.x, availableCell.cellPos.y];
-
-                        if (newCellPos.CheckIfOtherTeam(piece.PieceColor))
-                        {
-                            newCellPos.currentPiece.gameObject.SetActive(false);
-                        }*/
 
                         BoardState childBoardState = new BoardState(_maxDepth, _depth - 1, _parentState,
                             newCellGrid, piece, availableCell);
--- Assets/Scripts/MinimaxAI.cs
+++ Assets/Scripts/MinimaxAI.cs
@@ -10,15 +10,20 @@ using static Colours.ColourNames;
 
 public class MinimaxAI : MonoBehaviour
 {
-    private BoardManager _bm;
-    private BoardState _bestPossibleMove;
+    private BoardManager _bm; //Local reference to the BoardManager object
+    private BoardState _bestPossibleMove; //The final move the AI decides to do
     private int searchDepth = 7; //Increases search time exponentially
 
     private void Start()
     {
-        _bm = FindObjectOfType<BoardManager>();
+        _bm = BoardManager.Instance; //Gets the instance and stores it locally, saves calling the instance multiple times
     }
 
+    /// <summary>
+    /// Called when the AI does it's turn
+    /// Calls a recursive search through possible moves
+    /// Then moves the appropriate piece
+    /// </summary>
     public void DoTurn()
     {
         var currentBoardPosition = new BoardState(searchDepth,_bm.board.cellGrid,searchDepth);
@@ -34,29 +39,45 @@ public class MinimaxAI : MonoBehaviour
         
         if (_bestPossibleMove != null)
         {
+            //Finds the piece in the instantiated piece array
             Piece pieceToMove = _bm.blackPieces.Find(x => x == _bestPossibleMove.pieceToMove);
+            //Finds the cell where to move the piece to on the board
             Cell cellToMove = cellGrid[_bestPossibleMove.cellToMove.cellPos.x, _bestPossibleMove.cellToMove.cellPos.y];
 
+            //If there is a piece that can be taken
             if (cellToMove.CheckIfOtherTeam(pieceToMove.PieceColor))
             {
+                //Remove the piece from the board
                 cellToMove.currentPiece.gameObject.SetActive(false);
             }
 
+            //Move the piece to the cell
             pieceToMove.Place(cellToMove);
-            Debug.Log(cellToMove.cellPos);
         }
 
+        //Resets the move for the next turn
         _bestPossibleMove = null;
     }
 
-    //White is maximising, black is minimising
+    /// <summary>
+    /// Recursive search to find the best move
+    /// </summary>
+    /// <param name="depth">Number of moves it's looking ahead</param>
+    /// <param name="boardState">The future board state</param>
+    /// <param name="isMaximisingPlayer">Is it white</param>
+    /// <param name="alpha">Alpha value</param>
+    /// <param name="beta">Beta value</param>
+    /// <returns></returns>
     private float Minimax(int depth, BoardState boardState, bool isMaximisingPlayer, float alpha, float beta)
     {
+        //White is maximising, black is minimising
         if (depth == 1)
         {
+            //Gives the board state the piece and cell to move
             _bestPossibleMove = boardState.ParentState;
             _bestPossibleMove.pieceToMove = boardState.pieceToMove;
             _bestPossibleMove.cellToMove = boardState.cellToMove;
+            //Returns the static evaluation of the board state
             return CalculateStaticEvaluation(boardState.CellGrid, boardState);
         }
         
@@ -93,40 +114,56 @@ public class MinimaxAI : MonoBehaviour
         }
     }
     
+    /// <summary>
+    /// Calculates a score for the given grid
+    /// </summary>
+    /// <param name="cellGrid">Local array to store the board</param>
+    /// <param name="boardState">The board state to calculate the score of</param>
+    /// <returns></returns>
     private int CalculateStaticEvaluation(Cell[,] cellGrid, BoardState boardState)
     {
+        //Local score variable to modify and then return
         int score = 0;
+        //Loops through every cell on the board
         foreach (var cell in cellGrid)
         {
             if (cell.currentPiece != null)
             {
+                //If there is an active piece
                 if (cell.currentPiece.gameObject.activeSelf)
                 {
                     if (cell.currentPiece.PieceColor.Equals(Colours.ColourValue(White)))
                     {
+                        //Add the score of this piece
                         score += BoardManager.Instance.pieceEvaluation[cell.currentPiece.GetType()];
                     }
                     else if (cell.currentPiece.PieceColor.Equals(Colours.ColourValue(Black)))
                     {
+                        //Minus the score of this piece
                         score -= BoardManager.Instance.pieceEvaluation[cell.currentPiece.GetType()];
                     }
                 }
             }
         }
 
+        //Joins together the white and black piece list
         var allPieces = _bm.whitePieces.Concat(_bm.blackPieces);
-        Piece pieceToMove = allPieces.ToList().Find(x => x == _bestPossibleMove.pieceToMove);
-        Cell cellToMove = cellGrid[_bestPossibleMove.cellToMove.cellPos.x, _bestPossibleMove.cellToMove.cellPos.y];
+        //Finds the piece object
+        Piece pieceToMove = allPieces.ToList().Find(x => x == boardState.pieceToMove);
+        //Finds the cell
+        Cell cellToMove = cellGrid[boardState.cellToMove.cellPos.x, boardState.cellToMove.cellPos.y];
 
+        //If there's a piece that it can take
         if (cellToMove.CheckIfOtherTeam(pieceToMove.PieceColor))
         {
+            //Adjust the score accordingly
             if (cellToMove.currentPiece.PieceColor.Equals(Colours.ColourValue(White)))
             {
-                score += BoardManager.Instance.pieceEvaluation[cellToMove.currentPiece.GetType()];
+                score -= BoardManager.Instance.pieceEvaluation[cellToMove.currentPiece.GetType()];
             }
             else if (cellToMove.currentPiece.PieceColor.Equals(Colours.ColourValue(Black)))
             {
-                score -= BoardManager.Instance.pieceEvaluation[cellToMove.currentPiece.GetType()];
+                score += BoardManager.Instance.pieceEvaluation[cellToMove.currentPiece.GetType()];
             }
         }
         
