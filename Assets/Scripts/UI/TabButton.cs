e3e7a1f9ce551b8bdee75daf4ec79385acb541a3
Missing-Tech <josephgrabski31@gmail.com>
2020-11-10 16:22:22 +0000

    added options menu + tabs

---
+++ Assets/Scripts/UI/TabButton.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class TabButton : MonoBehaviour, IPointerEnterHandler, IPointerClickHandler, IPointerExitHandler
+{
+    public TabGroup tabGroup;
+    public Image background;
+    public TextMeshProUGUI text;
+    public GameObject tabArea;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        background = GetComponent<Image>();
+        text = GetComponentInChildren<TextMeshProUGUI>();
+        tabGroup.Subscribe(this);
+    }
+    
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        tabGroup.OnTabEnter(this);
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        tabGroup.OnTabSelected(this);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        tabGroup.OnTabExit(this);
+    }
+}
